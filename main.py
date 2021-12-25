a91f995afe24d1b5df9423c54f9c67d1a192fce0
ssssfssssfssssfs <91757057+ssssfssssfssssfs@users.noreply.github.com>
2021-12-25 13:49:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import asyncio
 from pytgcalls import idle
-from driver.amort import call_py, bot
+from driver.veez import call_py, bot
 
-async def mulai_bot():
-    print("[amort]: STARTING BOT CLIENT")
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    print("[amort]: STARTING PYTGCALLS CLIENT")
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
-    await pidle()
-    print("[amort]: STOPPING BOT & USERBOT")
+    print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(mulai_bot())
+loop.run_until_complete(start_bot())
