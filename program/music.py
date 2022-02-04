008f290d535420c6eb1cb412616119bc38f4b193
TeamArkon <97019316+TeamArkon@users.noreply.github.com>
2022-02-04 15:38:50 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -42,9 +42,9 @@ async def ytdl(format: str, link: str):
 @Client.on_message(command(["play", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
-    do = requests.get(f"https://api.telegram.org/bot5000541675:AAHlOwG6v-Dw5NrrtDn671PbNbf6BcaMaB4/getChatMember?chat_id=@TeamArkon&user_id={m.from_user.id}").text
+    do = requests.get(f"https://api.telegram.org/bot5267747918:AAGrWigGyKQGF75VeSvB4D4R-SF-mwKneWE/getChatMember?chat_id=@MMUC2&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        await m.reply_text("اشترك بقناة البوت لتستطيع تشغيل الاغاني \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n - @TeamArkon . ")
+        await m.reply_text("اشترك بقناة البوت لتستطيع تشغيل الاغاني \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n - @MMUC2 . ")
     else:
         replied = m.reply_to_message
         chat_id = m.chat.id
